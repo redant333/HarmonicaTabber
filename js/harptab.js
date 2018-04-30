4b59e2c16cff1f0be0fdcdcc572184346c3227f7
redant333 <redant333@gmail.com>
2018-04-30 16:01:50 +0200

    Implement isValidNote

--- js/harptab.js
+++ js/harptab.js
@@ -4,7 +4,26 @@ const harptab = (function () {
             return null;
         },
         isValidNote: function (note) {
-            return null;
+            let len = note.length;
+
+            if (len != 1 && len != 2) {
+                return false;
+            }
+
+            let lowercaseNote = note.toLowerCase();
+            let notes = "abcdefg";
+            let modifications = "#b";
+            if (!notes.includes(lowercaseNote[0])) {
+                return false;
+            }
+
+            if (len == 2) {
+                if (!modifications.includes(note[1])) {
+                    return false;
+                }
+            }
+
+            return true;
         },
         isValidTab: function (string, formatting) {
             return null;
