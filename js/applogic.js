d80bb8217254dd186461773fa5cae4ae04489184
Milomir Babic <redant333@gmail.com>
2019-05-12 14:28:37 +0200

    Add combobox for choosing the harmonica key

--- js/applogic.js
+++ js/applogic.js
@@ -3,10 +3,12 @@
  * 
  * @param {string} notesId ID of the textarea containing notes that should be translated
  * @param {string} tabsId ID of the textarea where translated tabs should be placed
+ * @param {string} keyId ID of the combobox with harmonica key
  */
-function translateNotes(notesId, tabsId) {
+function translateNotes(notesId, tabsId, keyId) {
     notesString = document.getElementById(notesId).value;
     tabsField = document.getElementById(tabsId);
+    key = document.getElementById(keyId).value;
 
-    tabsField.value = harptab.fromOctavelessNotesToTabs(notesString, "C");
+    tabsField.value = harptab.fromOctavelessNotesToTabs(notesString, key);
 }
